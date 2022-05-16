c9f24dd8c9317d47b09f28a21e9a9cc35eedec71
Dinuka-Dilshan <jadinukadilshan@gmail.com>
2022-05-16 23:31:59 +0530

    feat: image upload

--- app/src/main/java/com/example/blooddonation/SignUpForm.java
+++ app/src/main/java/com/example/blooddonation/SignUpForm.java
@@ -1,22 +1,33 @@
 package com.example.blooddonation;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 import java.util.regex.Pattern;
 
 public class SignUpForm extends AppCompatActivity {
@@ -32,7 +43,11 @@ public class SignUpForm extends AppCompatActivity {
     private EditText confirmPassword;
     private Button signUp;
     private TextView goBack;
+    ImageView img;
 
+
+    Uri imageUri;
+    StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,17 +64,25 @@ public class SignUpForm extends AppCompatActivity {
         confirmPassword = findViewById(R.id.txtConfirmPassword);
         signUp = findViewById(R.id.btnRegister);
         goBack = findViewById(R.id.txtGoback);
-
-
+        img = findViewById(R.id.imageView2);
 
 
         DAOUser DAO = new DAOUser();
 
+
+        img.setOnClickListener(v->{
+            selectImage();
+        });
+
+
+
+
         signUp.setOnClickListener(v -> {
+
 
             if (getStringValue(name).length() == 0) {
                 Toast.makeText(this, "Name cannot be empty", Toast.LENGTH_SHORT).show();
-            } else if (phone.getText().toString().trim().length() != 10  ) {
+            } else if (phone.getText().toString().trim().length() != 10) {
                 Toast.makeText(this, "Phone number is invalid", Toast.LENGTH_SHORT).show();
             } else if (getStringValue(address).length() == 0) {
                 Toast.makeText(this, "Address number cannot be empty", Toast.LENGTH_SHORT).show();
@@ -73,9 +96,12 @@ public class SignUpForm extends AppCompatActivity {
                 Toast.makeText(this, "Password cannot be empty", Toast.LENGTH_SHORT).show();
             } else if (!getStringValue(confirmPassword).equals(getStringValue(password))) {
                 Toast.makeText(this, "Password does not match", Toast.LENGTH_SHORT).show();
-            } else {
+            }else if(imageUri == null){
+                Toast.makeText(this, "Please select a photo", Toast.LENGTH_SHORT).show();
+            }
+            else {
 
-
+                uploadImage(getStringValue(userName));
                 int selectedId = radioGroup.getCheckedRadioButtonId();
 
 
@@ -85,13 +111,13 @@ public class SignUpForm extends AppCompatActivity {
                 DAO.checkAvailableUser(getStringValue(userName)).addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        if(snapshot.getChildrenCount()>0){
+                        if (snapshot.getChildrenCount() > 0) {
                             Toast.makeText(SignUpForm.this, "Username is not available", Toast.LENGTH_SHORT).show();
-                        }else{
-                            DAO.register(user).addOnSuccessListener(suc->{
-                                Toast.makeText(getApplicationContext(),"Registered ! Sign In to login",Toast.LENGTH_LONG).show();
-                                startActivity(new Intent(getApplicationContext(),SignIn.class));
-                            }).addOnFailureListener(er->{
+                        } else {
+                            DAO.register(user).addOnSuccessListener(suc -> {
+                                Toast.makeText(getApplicationContext(), "Registered ! Sign In to login", Toast.LENGTH_LONG).show();
+                                startActivity(new Intent(getApplicationContext(), SignIn.class));
+                            }).addOnFailureListener(er -> {
                                 Toast.makeText(SignUpForm.this, er.getMessage(), Toast.LENGTH_SHORT).show();
                             });
                         }
@@ -109,17 +135,65 @@ public class SignUpForm extends AppCompatActivity {
 
         });
 
-        goBack.setOnClickListener(v->{
-            startActivity(new Intent(getApplicationContext(),SignIn.class));
+        goBack.setOnClickListener(v -> {
+            startActivity(new Intent(getApplicationContext(), SignIn.class));
         });
 
+
+    }
+
+    private void uploadImage(String username) {
+
+
+
+        storageReference = FirebaseStorage.getInstance().getReference("images/" + username);
+
+
+        storageReference.putFile(imageUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                        img.setImageURI(null);
+                        Toast.makeText(getApplicationContext(), "Successfully Uploaded", Toast.LENGTH_SHORT).show();
+
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+
+                        Toast.makeText(getApplicationContext(), "Failed to Upload", Toast.LENGTH_SHORT).show();
+
+
+                    }
+                });
+    }
+
+    private void selectImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 100);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 100 && data != null && data.getData() != null){
+
+            imageUri = data.getData();
+            img.setImageURI(imageUri);
+
+
+        }
     }
 
     private String getStringValue(EditText txt) {
         return txt.getText().toString().trim();
     }
-
-
 
 
 }
