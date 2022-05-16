629d27531307a0238cadf5066e5cdfcc33b23254
Dinuka-Dilshan <jadinukadilshan@gmail.com>
2022-05-16 18:38:21 +0530

    feat: registration

--- app/src/main/java/com/example/blooddonation/DAOUser.java
+++ app/src/main/java/com/example/blooddonation/DAOUser.java
@@ -3,17 +3,27 @@ package com.example.blooddonation;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 public class DAOUser {
-    private  DatabaseReference databaseReference;
+    private DatabaseReference databaseReference;
 
 
     public DAOUser() {
         FirebaseDatabase db = FirebaseDatabase.getInstance("https://blooddonation-dc2ed-default-rtdb.asia-southeast1.firebasedatabase.app");
-        databaseReference =  db.getReference(User.class.getSimpleName());
+        databaseReference = db.getReference("User");
     }
 
-    public Task<Void> register (User user){
-       return  databaseReference.setValue(user);
+    public Task<Void> register(User user) {
+
+        return databaseReference.push().setValue(user);
+
     }
+
+    public Query checkAvailableUser(String userName) {
+
+        return databaseReference.orderByChild("userName").equalTo(userName);
+
+    }
+
 }
