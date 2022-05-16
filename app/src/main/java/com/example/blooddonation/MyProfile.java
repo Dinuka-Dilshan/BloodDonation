e193b540e47722e504e8adef8df6a6a382ca0050
Dinuka-Dilshan <jadinukadilshan@gmail.com>
2022-05-17 01:19:36 +0530

    feat:load image

--- app/src/main/java/com/example/blooddonation/MyProfile.java
+++ app/src/main/java/com/example/blooddonation/MyProfile.java
@@ -4,14 +4,23 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
 
 public class MyProfile extends AppCompatActivity {
 
@@ -21,6 +30,8 @@ public class MyProfile extends AppCompatActivity {
     EditText address;
     EditText district;
     EditText bloodGroup;
+    StorageReference storageRef;
+    ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,24 +40,39 @@ public class MyProfile extends AppCompatActivity {
 
         btn = findViewById(R.id.button);
         phone = findViewById(R.id.my_profile_contact_no);
-        name  =findViewById(R.id.my_profile_name);
+        name = findViewById(R.id.my_profile_name);
         address = findViewById(R.id.my_profile_address);
         district = findViewById(R.id.my_profile_district);
         bloodGroup = findViewById(R.id.my_profile_blood_group);
-
+        img = findViewById(R.id.my_prifile_image);
 
         Intent i = getIntent();
         String userName = i.getStringExtra("userName");
+
+        storageRef = FirebaseStorage.getInstance().getReference().child("images/"+userName);
+
+        final long ONE_MEGABYTE = 1024 * 1024;
+        storageRef.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+                img.setImageBitmap(BitmapFactory.decodeByteArray(bytes,0,bytes.length));
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle any errors
+            }
+        });
+
 
         DAOUser db = new DAOUser();
         db.checkAvailableUser(userName).addListenerForSingleValueEvent(new ValueEventListener() {
 
 
-
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
 
-                for(DataSnapshot ds : snapshot.getChildren()){
+                for (DataSnapshot ds : snapshot.getChildren()) {
                     name.setText(ds.child("name").getValue().toString());
                     phone.setText(ds.child("phoneNumber").getValue().toString());
                     address.setText(ds.child("address").getValue().toString());
@@ -62,6 +88,7 @@ public class MyProfile extends AppCompatActivity {
         });
 
     }
+
     private String getStringValue(EditText txt) {
         return txt.getText().toString().trim();
     }
