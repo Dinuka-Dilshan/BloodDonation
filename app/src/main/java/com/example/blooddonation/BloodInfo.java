20b768d2d43c004d44b5d344799b2b140172e2e3
LasithEranga <lasitheranga1@gmail.com>
2022-05-18 01:35:52 +0530

    drawer

--- app/src/main/java/com/example/blooddonation/BloodInfo.java
+++ app/src/main/java/com/example/blooddonation/BloodInfo.java
@@ -2,13 +2,32 @@ package com.example.blooddonation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class BloodInfo extends AppCompatActivity {
+
+    private Button btn_info;
+    private Button btnGetStarted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blood_info);
+
+        btn_info = findViewById(R.id.btn_info);
+        btnGetStarted = findViewById(R.id.btnGetStarted);
+
+
+        btn_info.setOnClickListener(v->{
+//            Intent i = new Intent(getApplicationContext(),Donors.class);
+//            startActivity(i);
+        });
+
+        btnGetStarted.setOnClickListener(v->{
+            Intent i = new Intent(getApplicationContext(),MainActivity.class);
+            startActivity(i);
+        });
     }
 }
