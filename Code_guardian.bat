0d9ec6b835d08a41d5bd5a36a350609e854d9ee4
LasithEranga <lasitheranga1@gmail.com>
2022-05-17 13:29:00 +0530

    Backup by bot

---
+++ Code_guardian.bat
@@ -0,0 +1,17 @@
+@echo off
+echo -------------------Welcome to github auto backup bot-------------------
+echo Current Configurations
+echo Command wating: 10 sec
+echo Backup frequency: 5Min
+SET /A "index = 1"
+SET /A "count = 10"
+:while
+if %index% leq %count% (
+   git add .
+   timeout 10
+   git commit -m "Backup by bot"
+   timeout 10
+   git push
+   timeout 300
+   goto :while
+)
