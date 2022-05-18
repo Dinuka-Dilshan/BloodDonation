e2b7cb8c3114862c4c04bf72f24a912fe59ccafe
LasithEranga <lasitheranga1@gmail.com>
2022-05-18 14:06:12 +0530

    validations

--- app/src/main/java/com/example/blooddonation/BloodInfo.java
+++ app/src/main/java/com/example/blooddonation/BloodInfo.java
@@ -29,35 +29,39 @@ public class BloodInfo extends AppCompatActivity {
 
 
 
+
         NavigationView nav = findViewById(R.id.nav_view);
         nav.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-                switch(item.getItemId()){
+                switch (item.getItemId()) {
                     case R.id.nav_home:
-                        startActivity(new Intent(getApplicationContext(),Home.class));
+                        startActivity(new Intent(getApplicationContext(), Home.class));
                         break;
 
                     case R.id.nav_bloodRequest:
-                        startActivity(new Intent(getApplicationContext(),BloodRequestActivity.class));
+                        startActivity(new Intent(getApplicationContext(), BloodRequestActivity.class));
                         break;
 
                     case R.id.nav_logout:
-                        startActivity(new Intent(getApplicationContext(),BloodInfo.class));
+                        startActivity(new Intent(getApplicationContext(), BloodInfo.class));
                         break;
 
                     case R.id.nav_myProfile:
-                        startActivity(new Intent(getApplicationContext(),MyProfile.class));
+                        startActivity(new Intent(getApplicationContext(), MyProfile.class));
                         break;
                     case R.id.nav_organization:
-                        startActivity(new Intent(getApplicationContext(),Organizations.class));
+                        startActivity(new Intent(getApplicationContext(), Organizations.class));
                         break;
                 }
                 DrawerLayout drawer = findViewById(R.id.drawer_layout);
                 drawer.closeDrawer(GravityCompat.START);
                 return false;
             }
+
+
+        });
 
         btn_info = findViewById(R.id.btn_info);
         btnGetStarted = findViewById(R.id.btnGetStarted);
